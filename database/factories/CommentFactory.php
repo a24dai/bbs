4dee66778e6acf7dfb1ae96d7c7d2c3fa3f2dc61
dai <a24arc.ad@gmail.com>
2019-04-25 15:25:37 +0900

    feat: post, comment factoryを記述

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        //
+        'body' => "コメントです。\nテキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
     ];
 });
+
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        //
+        'title' => 'タイトル',
+        'body'  => "本文です。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
     ];
 });
+
