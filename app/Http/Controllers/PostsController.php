e30aed0355aaed588110a139e9def3b2460eb79d
dai <a24arc.ad@gmail.com>
2019-04-25 16:30:33 +0900

    feat: PostsControllerのindexメソッド作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
-    //
+
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    public function index()
+    {
+        $posts = $this->post->orderBy('created_at', 'desc')->get();
+        return view('post.index', compact('posts'));
+    }
+
 }
+
