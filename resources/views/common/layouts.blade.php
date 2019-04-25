10ed47af4c0bbd895b2b766f83ccea1c44790cc7
dai <a24arc.ad@gmail.com>
2019-04-25 16:44:56 +0900

    feat: index画面と共通画面のview作成

---
+++ resources/views/common/layouts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>Laravel BBS</title>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    >
+  </head>
+  <body>
+    <header class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="{{ url('') }}">Laravel BBS</a>
+      </div>
+    </header>
+    <div>
+      @yield('content')
+    </div>
+  </body>
+</html>
