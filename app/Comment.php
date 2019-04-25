4557733697807ecf95ab20fda052d946f16e6315
dai <a24arc.ad@gmail.com>
2019-04-25 14:56:00 +0900

    feat: Comment, Postのモデルにfillableとrelation定義

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'body',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
+
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comment;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
+
